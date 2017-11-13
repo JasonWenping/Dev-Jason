8651fd33c8ff78b562d14cb781b0f41b91ea1a77
Jason <wenping_90@163.com>
2017-11-14 00:26:28 +0800

    day1

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,5 +1,23 @@
 function navList(){
+	var list = document.getElementById('top');
+	var link = list.getElementsByTagName('a');
+	link[0].style.color = '#00B7FF';
+	for (var i = 0; i < link.length; i++) {
+		link[i].onclick = function(i){
+			return function(){
+					link[i].style.color = '#00B7FF';
+					for (var j = 0; j<link.length;j++){
+						if (j != i){
+							link[j].style.color = '#FFF';
+							}
+						}
+
+				}
+			}(i);
+
+}
+	console.log(i);
 
 }
 
-addLoadEvent(navList);
+addLoadEvent(navList)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var router = require('koa-router')();
 
 router.get('/', function *(next) {
   yield this.render('index', {
-    title: 'Hello World Koa!'
+    title: 'CHARITY',
+    tag:['HOME','DISCOVWERY','START CAMPAIGN','BLOG','SHOP','CONTACT','...'],
+    slogen:'Helping People',
+    s_slogen:'This is a paleform for someone to CAMPAIGN the one whom needs help.It is absolutly free platform. Welcome your visit. ',
+    box_title:['SAVE LIVES','GIVE AN EDUCATION','CARE ABOUT HEALTH'],
+    box_text:'Not usderstand to ask me how to read myproject.',
+    live_title:'Live Campains',
+    live_text:'What ever,Just show some english sentence.SO listen to me to introduce this web to you.I hope you can fully have my words to usderstand this.',
+
   });
 });
 

cbf41cd8b77e2a68a0d751bce4da2c695459e3f1
Jason <wenping_90@163.com>
2017-11-14 22:41:59 +0800

    the info list and the picGallery

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,4 +1,5 @@
 function navList(){
+	//alert('dd');
 	var list = document.getElementById('top');
 	var link = list.getElementsByTagName('a');
 	link[0].style.color = '#00B7FF';
@@ -11,13 +12,71 @@ function navList(){
 							link[j].style.color = '#FFF';
 							}
 						}
-
 				}
 			}(i);
-
-}
+		}
 	console.log(i);
+	//shaddow();
+}
 
+var MouseActive = {
+	shaddowIcon:document.getElementsByClassName('shaddow_icon'),
+	shaddowImg:document.getElementsByClassName('shaddow_img'),
+	onMouseOver:function(i){
+			shaddowIcon[i].style.display = 'block';
+			shaddowImg[i].style.display = 'block';
+	},
+	onMouseOut:function(i){
+			shaddowIcon[i].style.display = 'null';
+			shaddowImg[i].style.display = 'null';
+	},
+};
+
+
+function shaddow(){
+	var box = document.getElementsByClassName('live_img');
+	//var image = box.getElementsByTagName('img');
+	//var mouse = new MouseActive();
+	var shaddowIcon = document.getElementsByClassName('shaddow_icon');
+	var shaddowImg = document.getElementsByClassName('shaddow_img');
+	for(var i = 0; i<shaddowImg.length; i++){
+		
+        /*
+		image[i].onmouseover = function(){
+			return mouse.onMouseOver(i);
+			console.log(i);
+		};
+		image[i].onmouseout = function(){
+			return mouse.onMouseOut(i);
+			console.log(i);
+		};
+		*/		
+		box[i].onmouseover = function(i){
+			return function(){
+				shaddowIcon[i].style.display = 'block';
+				shaddowImg[i].style.display = 'block';
+			}
+		}(i);
+		box[i].onmouseout = function(i){
+			return function(){
+				shaddowIcon[i].style.display = 'none';
+				shaddowImg[i].style.display = 'none';
+			}
+		}(i);
+	}
+	console.log(i);
+	/*
+	for (var i = 0;i<pannel.length;i++){
+		var imgs = pannel[i].getElementsByTagName('img');
+		imgs[0].onmouseover = function(){
+			for(var j = 0,j<shaddowImg.length;j++){
+				shaddowIcon[j].style.display = 'block';
+				shaddowImg[j].style.display = 'block';
+			}
+		}
+	}
+	*/
 }
 
 addLoadEvent(navList)
+addLoadEvent(shaddow)
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,11 @@ router.get('/', function *(next) {
     box_text:'Not usderstand to ask me how to read myproject.',
     live_title:'Live Campains',
     live_text:'What ever,Just show some english sentence.SO listen to me to introduce this web to you.I hope you can fully have my words to usderstand this.',
+    story_title:'Our Story',
+    story_text:'What ever,Just show some english sentence.SO listen to me to introduce this web to you.I hope you can fully have my words to usderstand this.',
+    l_title:['Urgen rebuilding - 2500 Chennai flood survivors','Internet Opens New World of Learning Opportunities','Support 8 youth to succeed	in school and society','Use Soap to Empower 60 Rape Survivors in Mail'],
+    l_text:'Sample Text is link shtt this heollo joke hah Next look loop Thanks!',
+    l_name:['India','Slerra Leone','Guaternala','Mall']
 
   });
 });
